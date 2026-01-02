e70ffe8fc3f76705f3317e2f6a96d5e355998d8b
blandnesz <dbiggle1@outlook.com>
2026-01-01 16:19:38 -0800

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-    prefix: '/static/tiw/',
-    bare:'https://bare.benrogo.net',
+    prefix: '/blandness/service/', // Make sure your repo name is here
+    bare: 'https://uv.radon.games/', // Or another working bare server
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/static/uv/uv.handler.js',
-    bundle: '/static/uv/uv.bundle.js',
-    config: '/static/uv/uv.config.js',
-    sw: '/static/uv/uv.sw.js',
-};
+    handler: '/blandness/uv/uv.handler.js',
+    bundle: '/blandness/uv/uv.bundle.js',
+    config: '/blandness/uv/uv.config.js',
+    sw: '/blandness/uv-sw.js', // Points to the file you moved
+}
